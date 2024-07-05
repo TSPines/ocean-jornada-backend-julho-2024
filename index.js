09509f41b3115a6152a2dcf306ccb0b632c94f51
BataTori <thammypinheiro@gmail.com>
2024-07-05 12:23:20 +0000

    Endpoint Atualização novo item

--- index.js
+++ index.js
@@ -43,4 +43,20 @@ app.get('/item/:id', function (req, res){
   res.send(item)
 })
 
+//Update - [PUT] /item/:id
+app.put('/item/:id', function (req, res){
+  //Acessamos o ID do parametro de rota
+  const id = req.params.id
+
+  //Acessamos o body da requisição, com os dados a serem atualizados
+  const novoItem = req.body.nome
+
+  //Atualizamos esse novoItem da lista, usando o Indice
+  lista[id - 1] = novoItem
+
+//Enviamos uma mensagem de sucesso
+  res.send('Item atualizado com sucesso: ' + id)
+})
+
+
 app.listen(3000)
